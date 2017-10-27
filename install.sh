3f2f18c0746ad814e71449f8fc188e35aeb65c58
corenel <xxdsox@gmail.com>
2017-10-27 16:25:22 +0800

    add albert amd apt-fast

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 # install essential packages
 sudo cp misc/sources.list /etc/apt/sources.list
+sudo cp misc/apt-fast.conf /etc/
 sudo mkdir -p /etc/apt/sources.list.d
 sudo add-apt-repository ppa:jonathonf/vim
 sudo add-apt-repository ppa:saiarcot895/myppa
+sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get -y install apt-fast
-sudo apt-fast install -y build-essential openssh-server git wget zsh htop vim python3-pip curl cmake autojump python-pip clang ccache pypy tmux polipo
+sudo apt-fast install -y build-essential openssh-server git wget zsh htop vim python3-pip curl cmake autojump python-pip clang ccache pypy tmux polipo albert
 
 # install oh-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - --no-check-certificate)"
